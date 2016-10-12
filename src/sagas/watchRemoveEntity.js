10c48f88d3acb57750aa3cf7d867a907869de303
Jan-Felix <janfelixschwarz@gmail.com>
2016-10-12 18:03:40 +0200

    Revert/delete body (#4)
    * Revert "Add `body` to `removeEntity`"
    
    This reverts commit 8d1a3a690a5dda6582bac34759af9c43e9314a93.
    
    * Revert "Add `body` to remove saga"
    
    This reverts commit cddce11cc516e604c33f6eae6e689d1ae91c22b6.

--- src/sagas/watchRemoveEntity.js
+++ src/sagas/watchRemoveEntity.js
@@ -9,7 +9,7 @@ import { deriveRequestIdFromAction } from '../utils'
 export function createRemoveEntity(types) {
   const actions = createActionCreators(types)
 
-  return function* removeEntity(type, requestId, query, body, getEntity, getPromise) {
+  return function* removeEntity(type, requestId, query, getPromise) {
     // const promise = getPromise(type, 'remove', { requestId })
 
     // if(promise.pending) {
@@ -17,14 +17,14 @@ export function createRemoveEntity(types) {
     // }
 
     const remove = getRemove(types, type)
-    yield put(actions.request(type, requestId, body))
+    yield put(actions.request(type, requestId))
 
     // TODO to be cool, do an optimistic remove of the entity cache and revert to
     // previous state stored in `entity` var if the request fails
 
-    const { response = {}, error } = yield call(remove, query, body)
+    const { error } = yield call(remove, query)
     if (!error) {
-      yield put(actions.success(type, requestId, response.result, response.entities))
+      yield put(actions.success(type, requestId))
     } else {
       yield put(actions.failure(type, requestId, error))
     }
@@ -33,14 +33,14 @@ export function createRemoveEntity(types) {
 
 export default function createWatchRemoveEntity(types) {
   const removeEntity = createRemoveEntity(types)
-
-  return function* watchRemoveEntity(getEntity, getPromise) {
+  return function* watchRemoveEntity(getPromise) {
     yield* takeEvery(
       REMOVE_ENTITY,
-      (action) => removeEntity(
-        action.payload.entity, deriveRequestIdFromAction(action),
-        action.payload.query, action.payload.body,
-        getEntity, getPromise
+      action => removeEntity(
+        action.payload.entity,
+        deriveRequestIdFromAction(action),
+        action.payload.query,
+        getPromise
       )
     )
   }
