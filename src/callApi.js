b9910cc504b909a8fa82ab7986edf87e485e0560
Jan-Felix <janfelixschwarz@gmail.com>
2016-11-07 10:45:25 +0100

    added support for lazily resolving urls (#5)

--- src/callApi.js
+++ src/callApi.js
@@ -4,7 +4,11 @@ import { normalize } from 'normalizr'
 // Fetches an API response and normalizes the result JSON according to schema.
 // This makes every API response have the same shape, regardless of how nested it was.
 export default function callApi(fullUrl, schema, options) {
-  return fetch(fullUrl, options)
+  const finalUrl = (typeof fullUrl === "function") ?
+    fullUrl() :
+    fullUrl
+  
+  return fetch(finalUrl, options)
     .then(response => {
       const contentType = response.headers.get('Content-Type') &&
         response.headers.get('Content-Type').split(';')[0]
