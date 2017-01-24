9093847c0fbf123f2f60be43949d0ab93b5c5f4c
Alexander Trefz <alexander.trefz@signavio.com>
2017-01-24 12:53:15 +0100

    Refactor utils/deriveRequestId

--- src/utils/deriveRequestId.js
+++ src/utils/deriveRequestId.js
@@ -1,9 +1,25 @@
-// TODO: maybe switch to a proper hashing to make sure to not have key
-// collision when queries with different key sets are used for the same type
-const stringifyQuery = (query) => JSON.stringify(query)
+import { chain } from 'lodash'
 
-export default (method, { query, elementId, propName }) => (
-  method === 'create'
-    ? `create_${elementId}_${propName}`
-    : `${method}_${stringifyQuery(query)}`
-)
+const stringifyQuery = (query) => {
+  const stringifiedQuery = chain(query)
+    .toPairs()
+    .sort()
+    .reduce(
+      (previous, current) =>
+        previous + JSON.stringify(current),
+      ''
+    )
+    .value()
+
+  return btoa(stringifiedQuery)
+}
+
+export default ({ method, query, ...options }) => {
+  const stringifiedQuery = stringifyQuery(query)
+
+  if (method === 'create') {
+    return `${method}_${stringifiedQuery}_${options.elementId}`
+  }
+
+  return `${method}_${stringifiedQuery}`
+}
