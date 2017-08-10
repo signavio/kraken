eb5b3d82e119c6262d2d8f915382541c1ad974c1
Hemerson Carlin <mersocarlin@gmail.com>
2017-08-10 16:52:57 +0200

    Optimistic updates (#32)
    * Add .vscode settings
    
    * Remove empty props from cache before update

--- test/specs/reducers/entityReducer.spec.js
+++ test/specs/reducers/entityReducer.spec.js
@@ -14,7 +14,10 @@ import { apiTypes, types, data } from '../fixtures'
 const actions = createActionCreators(apiTypes)
 
 const id = 'my-id'
-const requestId = deriveRequestIdFromAction({ type: actionTypes.FETCH_SUCCESS, payload: { query: { id } } })
+const requestId = deriveRequestIdFromAction({
+  type: actionTypes.FETCH_SUCCESS,
+  payload: { query: { id } },
+})
 const collection = typeUtils.getCollectionName(apiTypes, types.USER)
 const entityReducerForEntity = createEntitiesReducer(apiTypes, types.USER)
 
@@ -30,7 +33,7 @@ describe('entityReducer', () => {
           requestId,
           value: result,
           entities,
-        }),
+        })
       )
 
       expect(newState).to.deep.equal(entities[collection])
@@ -45,25 +48,168 @@ describe('entityReducer', () => {
           requestId,
           value: result,
           entities,
-        }),
+        })
       )
 
       // new request which does not return the activities
       const { firstName, ...partialUser } = entities.users['user-1']
+
       expect(firstName).to.exist
 
       const nextState = entityReducerForEntity(
         state,
-
         actions.succeedFetch({
           entityType: types.USER,
           requestId,
           value: result,
           entities: { 'user-1': partialUser },
-        }),
+        })
       )
 
       expect(nextState['user-1'].firstName).to.equal(firstName)
+    })
+  })
+
+  describe('UPDATE_SUCCESS', () => {
+    it('should update cache to new response', () => {
+      const state = entityReducerForEntity(
+        {},
+        actions.succeedFetch({
+          entityType: types.USER,
+          requestId,
+          value: result,
+          entities,
+        })
+      )
+
+      let nextState = entityReducerForEntity(
+        state,
+        actions.dispatchUpdate({
+          entityType: types.USER,
+          requestId,
+          body: {
+            id: 'user-1',
+            age: 10,
+          },
+        })
+      )
+
+      expect(nextState['user-1'].age).to.be.undefined
+
+      nextState = entityReducerForEntity(
+        nextState,
+        actions.succeedUpdate({
+          entityType: types.USER,
+          requestId,
+          value: result,
+          entities: {
+            users: {
+              'user-1': {
+                id: 'user-1',
+                firstName: 'John',
+                lastName: 'doe',
+                age: 10,
+              },
+            },
+          },
+        })
+      )
+
+      expect(nextState['user-1'].age).to.be.equal(10)
+    })
+
+    it('should remove empty entries from cache before update', () => {
+      const state = entityReducerForEntity(
+        {},
+        actions.succeedFetch({
+          entityType: types.USER,
+          requestId,
+          value: result,
+          entities,
+        })
+      )
+
+      let nextState = entityReducerForEntity(
+        state,
+        actions.dispatchUpdate({
+          entityType: types.USER,
+          requestId,
+          body: {
+            id: 'user-1',
+            firstName: 'John',
+            lastName: null,
+          },
+        })
+      )
+
+      expect(nextState['user-1'].lastName).to.be.null
+
+      nextState = entityReducerForEntity(
+        nextState,
+        actions.succeedUpdate({
+          entityType: types.USER,
+          requestId,
+          value: result,
+          entities: {
+            users: {
+              'user-1': {
+                id: 'user-1',
+                firstName: 'John',
+                lastName: null,
+              },
+            },
+          },
+        })
+      )
+
+      expect(nextState['user-1'].lastName).to.be.null
+    })
+
+    it('should remove empty entries from cache and update it with new value', () => {
+      const state = entityReducerForEntity(
+        {},
+        actions.succeedFetch({
+          entityType: types.USER,
+          requestId,
+          value: result,
+          entities,
+        })
+      )
+
+      let nextState = entityReducerForEntity(
+        state,
+        actions.dispatchUpdate({
+          entityType: types.USER,
+          requestId,
+          body: {
+            id: 'user-1',
+            firstName: 'John',
+            lastName: null,
+          },
+        })
+      )
+
+      expect(nextState['user-1'].lastName).to.be.null
+
+      nextState = entityReducerForEntity(
+        nextState,
+        actions.succeedUpdate({
+          entityType: types.USER,
+          requestId,
+          value: result,
+          entities: {
+            users: {
+              'user-1': {
+                id: 'user-1',
+                firstName: 'John',
+                lastName: 'DoeD',
+              },
+            },
+          },
+        })
+      )
+
+      expect(nextState['user-1'].lastName).to.be.equal('DoeD')
     })
   })
 
@@ -72,13 +218,12 @@ describe('entityReducer', () => {
       const state = entityReducerForEntity(
         {
           [id]: { id },
-          'someotherid': {},
+          someotherid: {},
         },
-
         actions.dispatchRemove({
           entityType: types.USER,
           query: { id },
-        }),
+        })
       )
 
       expect(state).to.not.have.key(id)
@@ -88,11 +233,10 @@ describe('entityReducer', () => {
     it('should select the entity to remove by comparing query params with entity attributes', () => {
       const state = entityReducerForEntity(
         { [id]: { foo: 'bar', baz: 1, boo: true } },
-
         actions.dispatchRemove({
           entityType: types.USER,
           query: { foo: 'bar', baz: 1 },
-        }),
+        })
       )
 
       expect(state).to.not.have.key(id)
