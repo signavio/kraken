9b6d2b6ed197202499f70bb11c0ffbe9c29c98a0
Philipp Giese <giese.philipp+git@gmail.com>
2017-12-15 16:00:53 +0100

    added integration test for update

---
+++ test/specs/integration/updateArguments.spec.js
@@ -0,0 +1,100 @@
+import React from 'react'
+import { compose, combineReducers, applyMiddleware, createStore } from 'redux'
+import createSagaMiddleware from 'redux-saga'
+import { Provider } from 'react-redux'
+
+import { stub } from 'sinon'
+import { mount } from 'enzyme'
+import { expect } from 'chai'
+
+import apiCreator from '../../../src'
+
+const entityType = 'TEST_TYPE'
+
+function configureStore(rootReducer, saga) {
+  const sagaMiddleware = createSagaMiddleware()
+
+  const finalCreateStore = compose(applyMiddleware(sagaMiddleware))(createStore)
+
+  const store = finalCreateStore(rootReducer)
+  sagaMiddleware.run(saga, store.getState)
+  return store
+}
+
+const App = () => <div />
+
+describe('Integration - update', () => {
+  let update
+  let createApp
+
+  beforeEach(() => {
+    update = stub().returns({})
+
+    const types = {
+      [entityType]: {
+        collection: 'test',
+        update,
+      },
+    }
+
+    const { reducer, saga, connect } = apiCreator(types)
+
+    const rootReducer = combineReducers({
+      kraken: reducer,
+    })
+
+    createApp = query => {
+      const ConnectedApp = connect(() => ({
+        updateSomething: {
+          type: entityType,
+          method: 'update',
+          query,
+        },
+      }))(App)
+
+      return mount(
+        <Provider store={configureStore(rootReducer, saga)}>
+          <ConnectedApp />
+        </Provider>
+      ).find(App)
+    }
+  })
+
+  it('should call the update action', () => {
+    const component = createApp()
+
+    expect(component).to.have.prop('updateSomething')
+
+    expect(update).to.not.have.been.called
+
+    component.props().updateSomething({ id: 'foo' })
+
+    expect(update).to.have.been.calledOnce
+  })
+
+  it('should pass query params to the update action', () => {
+    const query = { foo: 'bar' }
+
+    const component = createApp(query)
+
+    expect(update).to.not.have.been.called
+
+    component.props().updateSomething({ id: 'foo' })
+
+    expect(update).to.have.been.calledOnce
+    expect(update).to.have.been.calledWith(query)
+  })
+
+  it('should pass the body to the update action', () => {
+    const body = { id: 'foo' }
+
+    const component = createApp()
+
+    expect(update).to.not.have.been.called
+
+    component.props().updateSomething(body)
+
+    expect(update).to.have.been.calledOnce
+    expect(update).to.have.been.calledWith({}, body)
+  })
+})
