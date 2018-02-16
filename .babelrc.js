627c0a59e20a7fe1d29790ae74f08bcafb2e4a7d
Philipp Giese <giese.philipp+git@gmail.com>
2018-02-16 15:33:32 +0100

    Improvement/babel lodash (#58)
    * added new jobs to also build es6 style
    
    * do not use chain from lodash
    
    * updated build to also produce module build
    
    * do not nest preset deeper than it should

---
+++ .babelrc.js
@@ -0,0 +1,23 @@
+const { BABEL_ENV } = process.env
+
+module.exports = {
+  presets: [
+    [
+      'env',
+      {
+        modules: BABEL_ENV === 'es6' ? false : 'commonjs',
+        targets: {
+          browsers: [
+            'chrome >= 50',
+            'firefox >= 52',
+            'safari >= 10',
+            'ie >= 11',
+          ],
+        },
+      },
+    ],
+    'stage-0',
+    'react',
+  ],
+  plugins: ['transform-runtime', 'lodash'],
+}
