bbbdd73dbbaa3148285c450d0e5b7cc2897750b5
Philipp Giese <giese.philipp+git@gmail.com>
2018-02-16 14:10:05 +0100

    do not nest preset deeper than it should

--- .babelrc.js
+++ .babelrc.js
@@ -1,25 +1,23 @@
 const { BABEL_ENV } = process.env
 
 module.exports = {
-  presets: {
-    presets: [
-      [
-        'env',
-        {
-          modules: BABEL_ENV === 'es6' ? false : 'commonjs',
-          targets: {
-            browsers: [
-              'chrome >= 50',
-              'firefox >= 52',
-              'safari >= 10',
-              'ie >= 11',
-            ],
-          },
+  presets: [
+    [
+      'env',
+      {
+        modules: BABEL_ENV === 'es6' ? false : 'commonjs',
+        targets: {
+          browsers: [
+            'chrome >= 50',
+            'firefox >= 52',
+            'safari >= 10',
+            'ie >= 11',
+          ],
         },
-      ],
-      'stage-0',
-      'react',
+      },
     ],
-  },
+    'stage-0',
+    'react',
+  ],
   plugins: ['transform-runtime', 'lodash'],
 }
