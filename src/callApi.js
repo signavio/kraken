c4b641a6126c04aaa7bf1f7b732bb7346f60f3f1
Christian W <SilentGert@users.noreply.github.com>
2018-05-04 14:47:48 +0200

    Added status code to dispatched request failures (#131)

--- src/callApi.js
+++ src/callApi.js
@@ -5,13 +5,11 @@ import { bustRequest } from './utils'
 // Fetches an API response and normalizes the result JSON according to schema.
 // This makes every API response have the same shape, regardless of how nested it was.
 export default function callApi(fullUrl, schema, options) {
-  const url = (typeof fullUrl === 'function') ?
-    fullUrl() :
-    fullUrl
+  const url = typeof fullUrl === 'function' ? fullUrl() : fullUrl
   const finalUrl = bustRequest(url, options)
 
   return fetch(finalUrl, options)
-    .then((response) => {
+    .then(response => {
       let contentType = response.headers.get('Content-Type')
 
       if (contentType !== null) {
@@ -20,9 +18,11 @@ export default function callApi(fullUrl, schema, options) {
 
       switch (contentType) {
         case 'application/json':
-          return response.json().then((json) => ({ json, response }))
+          return response.json().then(json => ({ json, response }))
         case 'text/plain':
-          return response.text().then((text) => ({ json: { message: text }, response }))
+          return response
+            .text()
+            .then(text => ({ json: { message: text }, response }))
         default:
           if (response.status === 204) {
             return { response }
@@ -42,12 +42,17 @@ export default function callApi(fullUrl, schema, options) {
       return json ? normalize(json, schema) : null
     })
     .then(
-      (response) => ({ response }),
-      ({ json, response, message }) => ({
-        error: message ?
-          `Error parsing the response: ${message}` :
-            (json && json.message) ||
-            (response && response.statusText),
-      })
+      response => ({ response }),
+      ({ json, response, message }) => {
+        const result = {
+          error: message
+            ? `Error parsing the response: ${message}`
+            : (json && json.message) || (response && response.statusText),
+        }
+        if (response && response.status) {
+          result.status = response.status
+        }
+        return result
+      }
     )
 }
