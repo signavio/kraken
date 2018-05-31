0c569753f7a3a8de7df3c050048a762419879045
Philipp Giese <giese.philipp+git@gmail.com>
2018-05-31 18:13:46 +0200

    remove deleted handles entities different than arrays

--- src/cachePolicies/removeReferencesToDeletedEntities.js
+++ src/cachePolicies/removeReferencesToDeletedEntities.js
@@ -1,20 +1,39 @@
 // @flow
 import { keys, intersection } from 'lodash'
 
-import type { Request, EntityCollectionT } from '../internalTypes'
+import type {
+  Request,
+  EntityCollectionT,
+  ApiTypeMap,
+  EntityType,
+} from '../internalTypes'
+
+import { hasEntitySchema } from '../types'
 
 // clean result values from ids that are no longer existent in the cache
-const removeDeleted = (request: Request, collection: EntityCollectionT) => {
+const removeDeleted = (
+  apiTypes: ApiTypeMap,
+  request: Request,
+  collection: EntityCollectionT,
+  entityType: EntityType
+) => {
   if (!request.value) {
     return request
   }
-  const existingIds = intersection(request.value, keys(collection))
-  return existingIds.length === request.value.length
-    ? request
-    : {
-        ...request,
-        value: existingIds,
-      }
+
+  if (hasEntitySchema(apiTypes, entityType)) {
+    const isInCollection = !!collection[request.value]
+
+    return isInCollection ? request : { ...request, value: undefined }
+  } else {
+    const existingIds = intersection(request.value, keys(collection))
+    return existingIds.length === request.value.length
+      ? request
+      : {
+          ...request,
+          value: existingIds,
+        }
+  }
 }
 
 export default {
