42cf5f87d3e32c0e25802bf03cc7de5a1bf7fa1f
Herman Dahlén <hman.dahlen@gmail.com>
2018-09-03 16:01:24 +0200

    more tests

--- test/specs/callApi.spec.js
+++ test/specs/callApi.spec.js
@@ -1,13 +1,31 @@
+import fetchMock from 'fetch-mock'
+import { startsWith } from 'lodash/fp'
+
 import expect from '../expect'
 
 import callApi from '../../src/callApi'
 
 describe('callApi', () => {
+  afterEach(() => {
+    fetchMock.restore()
+  })
   it('should return error when no absolute url provided', async () => {
     const response = await callApi('')
 
     expect(response).to.eql({
       error: 'Error parsing the response: only absolute urls are supported',
     })
+  })
+
+  it('should return response with result, responseHeaders and entities', async () => {
+    fetchMock.get(startsWith('/comments'), {
+      body: [{ id: '1', body: 'My first comment' }],
+    })
+
+    const { response } = await callApi('/comments', {}, {})
+
+    expect(response).to.have.property('result')
+    expect(response).to.have.property('responseHeaders')
+    expect(response).to.have.property('entities')
   })
 })
