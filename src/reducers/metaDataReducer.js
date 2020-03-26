4d72f3a732bd3b2addbdee8a7ee31edc951c9561
Philipp Giese <giese.philipp+git@gmail.com>
2020-03-26 08:53:30 +0100

    add default credentials

--- src/reducers/metaDataReducer.js
+++ src/reducers/metaDataReducer.js
@@ -3,7 +3,7 @@ import { ADD_META_DATA } from '../actions'
 import { type Action, type MetaData } from '../flowTypes'
 
 function metaReducer(
-  state: MetaData = Object.freeze({}),
+  state: MetaData = Object.freeze({ credentials: 'same-origin' }),
   action: Action
 ): MetaData {
   switch (action.type) {
